b8bbce51d6a7ac7ca970c8f73a91345e795494fd
Jonnie Spratley <jonniespratley@me.com>
2014-07-01 22:58:01 -0700

    updated code

--- test/test-creation.js
+++ test/test-creation.js
@@ -1,80 +1,67 @@
 'use strict';
-var path = require('path');
-var helpers = require('yeoman-generator').test;
-require('chai').should();
+var path = require( 'path' );
+var helpers = require( 'yeoman-generator' ).test;
+require( 'chai' ).should();
 
+describe( 'jps-site generator', function () {
 
-describe('jps-site generator', function() {
+	//Mocked questions and answers
+	var mockAnswers = {
+		siteTitle: 'My Test Site',
+		siteDesc: 'A modern site build to test.',
+		featureTitle: 'Mocha Tests',
+		featureBody: 'A modern site created by a Yeoman generator.',
+		featureImage: 'images/feature.png'
+	};
 
-  //Mocked questions and answers
-  var mockAnswers = {
-    siteTitle: 'My Test Site',
-    siteDesc: 'A modern site build to test.',
-    featureTitle: 'Mocha Tests',
-    featureBody: 'A modern site created by a Yeoman generator.',
-    featureImage: 'images/feature.png'
-  };
-  
-  //Mock expected files
-  var mockFiles = [
-    '.bowerrc', 
-    '.editorconfig', 
-    '.gitattributes', 
-    '.gitignore', 
-    '.jshintrc', 
-    '.travis.yml', 
-    'bower.json', 
-    'Gruntfile.js', 
-    'package.json',
-    'app/images/feature.png', 
-    'app/styles/main.css', 
-    'app/scripts/config.js',  
-    'app/scripts/main.js', 
-    'app/index.html'
-  ];
-  
-  //Mock expected file contents
-  var mockFilePairs = [
-    [ 'app/index.html', RegExp("<title>" + mockAnswers.siteTitle + "</title>") ], 
-    [ 'app/pages/main.html', RegExp("<h1>" + mockAnswers.featureTitle + "</h1>") ], 
-    [ 'app/pages/main.html', RegExp("" + mockAnswers.featureBody) ]
-  ];
+	//Mock expected files
+	var mockFiles = [
+		'.bowerrc',
+		'.editorconfig',
+		'.gitattributes',
+		'.gitignore',
+		'.jshintrc',
+		'.travis.yml',
+		'bower.json',
+		'Gruntfile.js',
+		'package.json',
+		'app/images/feature.png',
+		'app/styles/main.css',
+		'app/scripts/config.js',
+		'app/scripts/main.js',
+		'app/index.html'
+	];
 
-  /**
-    Before each test clean the test/temp folder and create a new generator.
-   */
-  beforeEach(function(done) {
-    helpers.testDirectory(path.join(__dirname, 'temp'), (function(err) {
-      if (err) {
-        done(err);
-      }
-      this.app = helpers.createGenerator('jps-site:app', ['../../app']);
-      done();
-    }).bind(this));
-  });
+	//Mock expected file contents
+	var mockFilePairs = [
+		['app/index.html', RegExp( "<title>" + mockAnswers.siteTitle + "</title>" )],
+		['app/pages/main.html', RegExp( "<h1>" + mockAnswers.featureTitle + "</h1>" )],
+		['app/pages/main.html', RegExp( "" + mockAnswers.featureBody )]
+	];
 
-  /**
-     First test if the files that were created match what we expect.
-   */
-  it('creates expected files', function(done) {
+	//Before each test clean the test/temp folder and create a new generator.
+	beforeEach( function (done) {
+		helpers.testDirectory( path.join( __dirname, 'temp' ), (function (err) {
+			if (err) {
+				done( err );
+			}
+			this.app = helpers.createGenerator( 'jps-site:app', ['../../app'] );
+			done();
+		}).bind( this ) );
+	} );
 
-    /**
-         Add some mock prompts for the user to answer
-     */
-    helpers.mockPrompt(this.app, mockAnswers);
+	//First test if the files that were created match what we expect.
+	it( 'creates expected files', function (done) {
 
-    /**
-         Skip installing of the bower and npm dependencies
-     */
-    this.app.options['skip-install'] = true;
-
-    /**
-         Run the app and test for files and file contents
-     */
-    this.app.run({}, function() {
-      helpers.assertFile(mockFiles);
-      helpers.assertFileContent(mockFilePairs);
-      done();
-    });
-  });
-});
+		//Add some mock prompts for the user to answer
+		helpers.mockPrompt( this.app, mockAnswers );
+		//Skip installing of the bower and npm dependencies
+		this.app.options['skip-install'] = true;
+		//Run the app and test for files and file contents
+		this.app.run( {}, function () {
+			helpers.assertFile( mockFiles );
+			helpers.assertFileContent( mockFilePairs );
+			done();
+		} );
+	} );
+} );
