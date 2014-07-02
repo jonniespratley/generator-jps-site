5d4b64d016f6e71059e5cefcae5084f8b191d750
Jonnie Spratley <jonniespratley@me.com>
2014-07-01 22:59:05 -0700

    updated code

--- test/test-creation.js
+++ test/test-creation.js
@@ -3,41 +3,41 @@ var path = require( 'path' );
 var helpers = require( 'yeoman-generator' ).test;
 require( 'chai' ).should();
 
+//Mocked questions and answers
+var mockAnswers = {
+	siteTitle: 'My Test Site',
+	siteDesc: 'A modern site build to test.',
+	featureTitle: 'Mocha Tests',
+	featureBody: 'A modern site created by a Yeoman generator.',
+	featureImage: 'images/feature.png'
+};
+
+//Mock expected files
+var mockFiles = [
+	'.bowerrc',
+	'.editorconfig',
+	'.gitattributes',
+	'.gitignore',
+	'.jshintrc',
+	'.travis.yml',
+	'bower.json',
+	'Gruntfile.js',
+	'package.json',
+	'app/images/feature.png',
+	'app/styles/main.css',
+	'app/scripts/config.js',
+	'app/scripts/main.js',
+	'app/index.html'
+];
+
+//Mock expected file contents
+var mockFilePairs = [
+	['app/index.html', RegExp( "<title>" + mockAnswers.siteTitle + "</title>" )],
+	['app/pages/main.html', RegExp( "<h1>" + mockAnswers.featureTitle + "</h1>" )],
+	['app/pages/main.html', RegExp( "" + mockAnswers.featureBody )]
+];
+
 describe( 'jps-site generator', function () {
-
-	//Mocked questions and answers
-	var mockAnswers = {
-		siteTitle: 'My Test Site',
-		siteDesc: 'A modern site build to test.',
-		featureTitle: 'Mocha Tests',
-		featureBody: 'A modern site created by a Yeoman generator.',
-		featureImage: 'images/feature.png'
-	};
-
-	//Mock expected files
-	var mockFiles = [
-		'.bowerrc',
-		'.editorconfig',
-		'.gitattributes',
-		'.gitignore',
-		'.jshintrc',
-		'.travis.yml',
-		'bower.json',
-		'Gruntfile.js',
-		'package.json',
-		'app/images/feature.png',
-		'app/styles/main.css',
-		'app/scripts/config.js',
-		'app/scripts/main.js',
-		'app/index.html'
-	];
-
-	//Mock expected file contents
-	var mockFilePairs = [
-		['app/index.html', RegExp( "<title>" + mockAnswers.siteTitle + "</title>" )],
-		['app/pages/main.html', RegExp( "<h1>" + mockAnswers.featureTitle + "</h1>" )],
-		['app/pages/main.html', RegExp( "" + mockAnswers.featureBody )]
-	];
 
 	//Before each test clean the test/temp folder and create a new generator.
 	beforeEach( function (done) {
@@ -52,7 +52,6 @@ describe( 'jps-site generator', function () {
 
 	//First test if the files that were created match what we expect.
 	it( 'creates expected files', function (done) {
-
 		//Add some mock prompts for the user to answer
 		helpers.mockPrompt( this.app, mockAnswers );
 		//Skip installing of the bower and npm dependencies
